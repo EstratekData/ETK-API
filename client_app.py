995e8f5794a0ebdbb30cf4ab4e5e4cc41b22bea2
EstratekData <estratekdata@gmail.com>
2023-04-17 16:17:08 -0400

    First Commit ETK API Server

---
+++ client_app.py
@@ -0,0 +1,29 @@
+import requests
+
+# Define the base URL for the API server
+base_url = "http://localhost:5000"
+
+# Define the endpoints we want to consume
+time_endpoint        = base_url + "/time"
+date_endpoint        = base_url + "/date"
+etk_finance_endpoint = base_url + "/etk_finance"
+
+# Send requests to the API server and print the responses
+time_response = requests.get(time_endpoint)
+print("Current time:", time_response.text)
+
+date_response = requests.get(date_endpoint)
+print("Current date:", date_response.text)
+
+etk_ticker_list_response = requests.get(etk_finance_endpoint)
+if etk_ticker_list_response.status_code == 200:
+    print (etk_ticker_list_response.text)
+    try:
+        print("Lista de Tickers: ")
+        print(etk_ticker_list_response.json())
+    except ValueError as e:
+        print("Error: Unable to decode response as JSON.")
+        print("Response content:", etk_ticker_list_response.content)
+else:
+    print("Error: Unable to retrieve list of strings.")
+
---
+++ etk_apiserver.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify
+from time1 import get_time
+from date import get_date
+import etk_stock
+import pandas as pd
+
+
+etk_apiserver = Flask(__name__)
+
+
+# ETK SERVER API ENDPOINTS 
+# ------------------------
+@etk_apiserver.route('/')
+def hello_world():
+    return '¡Hola, mundo!'
+
+@etk_apiserver.route('/date')
+def date():
+    print('API Server>> La fecha de hoy es: ')
+    return get_date()
+
+@etk_apiserver.route('/time')
+def time():
+    print('API Server>> La hora local es: ')
+    return get_time()
+
+@etk_apiserver.route('/etk_finance')
+def etk_get_tickers():
+    STOP_COUNT = 10
+    myData = etk_stock.etk_finance('output_plus.xlsx') 
+
+    # 1. Get the ticker lists from the URLs
+    nyse_tickers_list = myData.get_tickers('NYSE')
+    print ('NYSE Tickers List')
+    nyse_tickers_df = pd.DataFrame(nyse_tickers_list, columns=['Ticker', 'Ticker Name', 'Exchange'])
+    print(nyse_tickers_list)
+    return jsonify(nyse_tickers_list)
+
+
+
+
+if __name__ == '__main__':
+    print('Starting Estratek API server...')
+    etk_apiserver.run()
